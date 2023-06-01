d7deadd8d7d08ccf1bf881513cb2a0482012003d
leechanggi <67743720+leechanggi@users.noreply.github.com>
2023-06-01 18:53:51 +0900

    prettierrc

--- src/index.js
+++ src/index.js
@@ -5,20 +5,13 @@ import App from "./App";
 import AuthService from "./service/auth";
 import TweetService from "./service/tweet";
 import { BrowserRouter } from "react-router-dom";
-import {
-  AuthErrorEventBus,
-  AuthProvider,
-  fetchToken,
-  fetchCsrfToken,
-} from "./context/AuthContext";
+import { AuthErrorEventBus, AuthProvider, fetchToken, fetchCsrfToken } from "./context/AuthContext";
 import HttpClient from "./network/http";
 import Socket from "./network/socket";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 const authErrorEventBus = new AuthErrorEventBus();
-const httpClient = new HttpClient(BASE_URL, authErrorEventBus, () =>
-  fetchCsrfToken()
-);
+const httpClient = new HttpClient(BASE_URL, authErrorEventBus, () => fetchCsrfToken());
 const authService = new AuthService(httpClient);
 const socketClient = new Socket(BASE_URL, () => fetchToken());
 const tweetService = new TweetService(httpClient, socketClient);
@@ -26,10 +19,7 @@ const tweetService = new TweetService(httpClient, socketClient);
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider
-        authService={authService}
-        authErrorEventBus={authErrorEventBus}
-      >
+      <AuthProvider authService={authService} authErrorEventBus={authErrorEventBus}>
         <App tweetService={tweetService} />
       </AuthProvider>
     </BrowserRouter>
